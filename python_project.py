ccff9e42fefa5a244827654da21778065297a912
carriegrossman <66217922+carriegrossman@users.noreply.github.com>
2020-06-08 16:07:43 -0400

    Update python_project.py

--- python_project.py
+++ python_project.py
@@ -20,39 +20,4 @@ recipes = {
                 'amount':'2'
             }
         ]
-    },
-    "jrr-2":{
-        'name':'Steak',
-        'ingredients':[
-            {
-                'name':'New York Strip',
-                'amount':'12 oz'
-            },{
-                'name':'butter',
-                'amount':'1 tbsp'
-            },{
-                'name':'salt',
-                'amount':'2 pinches'
-            },{
-                'name':'pepper',
-                'amount':'1 pinch'
-            }
-        ]
-    },
-    "jrr-3":{
-        'name':'Fried Pork Chops',
-        'ingredients':[
-            {
-                'name':'Boneless Pork Chops',
-                'amount':'1 lb'
-            },{
-                'name':'Shake n\' Bake',
-                'amount':'1 packet'
-            },{
-                'name':'oil',
-                'amount':'1 cup'
-            }
-        ]
     }
-}
-
